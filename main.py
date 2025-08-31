23ca0ab78992b82c4a3e5e5488f6ebdd815fb25d
developertg88 <botstg88@gmail.com>
2025-08-31 19:09:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiohttp import web
+
+TOKEN = "8492187656:AAFVYToh_AzzoEJC7Q3_5LqoqX-zcv9m9ZM"
+CHANNEL_LINK = "https://t.me/+O6gnTNLs2B45YmYy"
+PHOTO_URL = "https://i.ibb.co/WNTmsNwt/photo.jpg"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    text = (
+        "Привет! Я тиктокер с опросами, мемами и приколами.\n\n"
+        f"Вот канал с опросами и мемами: {CHANNEL_LINK}"
+    )
+    await bot.send_photo(message.chat.id, PHOTO_URL, caption=text)
+
+# Веб-сервер для Render
+async def handle(request):
+    return web.Response(text="Bot is alive!")
+
+app = web.Application()
+app.add_routes([web.get("/", handle)])
+
+async def start_web():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 10000)
+    await site.start()
+
+async def main():
+    await start_web()
+    await dp.start_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
